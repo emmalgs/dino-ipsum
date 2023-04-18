27f9ecbefc1d0fe74b9ea2dbedd0f9d1f8b8faa4
Emma Gerigscott <elgerig@gmail.com>
2023-04-18 11:02:32 -0700

    add img file and package manager for img files, and inport CSS
     Co-authored-by: raed alkhanbashi <raed.alkhanbashi@gmail.com>
    dquote> co-authored-by: Kymani Stephens <kymanist00@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import DinoService from "./dino-service";
+import './css/styles.css';
 
 function getRandomDino() {
   let promise = DinoService.getDino();
   promise.then(function(dino) {
+    reset(); 
     printElements(dino);
+    console.log(dino);
     createDinoNameInputs(dino);
   }, function(errorMessage) {
     printError(errorMessage);
@@ -16,14 +19,32 @@ function createDinoNameInputs(dino) {
   dinoArray.forEach((letter) => {
     let input = document.createElement("input");
     input.setAttribute("type", "text");
+    input.maxLength = "1";
     input.dataset.id = letter;
     div.append(input);
   });
 }
 
+function checkDinoLetter(e) {
+  e.preventDefault();
+  let letters = document.querySelectorAll("input[type='text']");
+  letters.forEach((letter) => {
+    console.log(letter);
+    if (letter.value.toUpperCase() === letter.dataset.id) {
+      letter.style.backgroundColor = "green";
+    } else {
+      letter.style.backgroundColor = "red";
+    }
+  });
+}
+
 function printElements(result) {
   document.querySelector('#description').innerText = result.Description;
+}
 
+function reset() {
+  const div = document.querySelector('.letter-inputs')
+  div.innerHTML = null;
 }
 
 function printError(error) {
@@ -32,4 +53,5 @@ function printError(error) {
 
 window.addEventListener("load", function() {
   this.document.querySelector("button.load-dino").addEventListener("click", getRandomDino);
+  this.document.querySelector("#letters").addEventListener("submit", checkDinoLetter);
 });
