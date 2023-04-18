837f50cc99924e86b103bdc3defc3887acabf457
Emma Gerigscott <elgerig@gmail.com>
2023-04-18 12:12:17 -0700

    edit styles.css and modify index.html
     Co-authored-by: raed alkhanbashi <raed.alkhanbashi@gmail.com>
    dquote> co-authored-by: Kymani Stephens <kymanist00@gmail.com>

--- src/dino-service.js
+++ src/dino-service.js
@@ -3,12 +3,17 @@ export default class DinoService {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       const url = `https://dinosaur-facts-api.shultzlab.com/dinosaurs/random`;
+      
       request.addEventListener("loadend", function() {
-        const response = JSON.parse(this.responseText);
-        if (this.status === 200) {
-          resolve(response);
-        } else {
-          reject(this);
+        try {         
+          const response = JSON.parse(this.responseText);
+          if (this.status === 200) {
+            resolve(response);
+          } else {
+            reject(this);
+          }
+        } catch (error) {
+          return error;
         }
       });
       request.open("GET", url, true);
--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,15 @@ module.exports = {
           'html-loader'
         ]
       },
-      
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/fonts',}}]
+      },
       {
         test: /\.css$/,
         use: [
